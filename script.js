81295040aa5ec321b3a81d65ce7fefe9f5f92df3
Nos <zvezdazanatih@gmail.com>
2025-05-05 11:24:59 +0700

    10

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const form1 = document.getElementById('form1');
   const formData = new FormData(form1); // Сбор данных формы
   const FormDataObject = Object.fromEntries(formData);
 
-  fetch('http://truruki.ru/add', {
+  fetch('http://truruki.ru/api/add', {
     method: 'POST',
     body: JSON.stringify({
       ...FormDataObject,
